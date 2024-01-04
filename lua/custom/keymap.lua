1acd39050d3cabef911e919ddb1ab10ee8696c33
Linus Bensryd <linus.bensryd@gmail.com>
2024-01-04 15:56:18 +0100

    I guess lua format wanted ' instead of " around strings

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -2,8 +2,8 @@ local function map(mode, l, r, desc)
   vim.keymap.set(mode, l, r, { desc = desc })
 end
 
-map("n", "[b", "<cmd>bprevious<cr>", "Prev buffer")
-map("n", "]b", "<cmd>bnext<cr>", "Next buffer")
+map('n', '[b', '<cmd>bprevious<cr>', 'Prev buffer')
+map('n', ']b', '<cmd>bnext<cr>', 'Next buffer')
 
 local diagnostic_goto = function(next, severity)
   local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
@@ -13,12 +13,12 @@ local diagnostic_goto = function(next, severity)
   end
 end
 
-map("n", "]d", diagnostic_goto(true), "Next Diagnostic")
-map("n", "[d", diagnostic_goto(false), "Prev Diagnostic")
-map("n", "]e", diagnostic_goto(true, "ERROR"), "Next Error")
-map("n", "[e", diagnostic_goto(false, "ERROR"), "Prev Error")
-map("n", "]w", diagnostic_goto(true, "WARN"), "Next Warning")
-map("n", "[w", diagnostic_goto(false, "WARN"), "Prev Warning")
+map('n', ']d', diagnostic_goto(true), 'Next Diagnostic')
+map('n', '[d', diagnostic_goto(false), 'Prev Diagnostic')
+map('n', ']e', diagnostic_goto(true, 'ERROR'), 'Next Error')
+map('n', '[e', diagnostic_goto(false, 'ERROR'), 'Prev Error')
+map('n', ']w', diagnostic_goto(true, 'WARN'), 'Next Warning')
+map('n', '[w', diagnostic_goto(false, 'WARN'), 'Prev Warning')
 
-map("n", "Q", "@qj")
-map("x", "Q", ":norm @q<cr>")
+map('n', 'Q', '@qj')
+map('x', 'Q', ':norm @q<cr>')
